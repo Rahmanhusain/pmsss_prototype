a5e606d38dcb55b3861296d6debda670992c767a
Rahmanhusain <rahmanhusain899@gmail.com>
2024-09-15 12:45:42 +0530

    camera retake update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,15 @@
 import Image from "next/image";
 import Onepage from "@/Components/Onepage";
-import Header from '@/Components/Header';
+import Header from "@/Components/Header";
 import Capture from "@/Components/Capture";
 // import Image from 'next/image';
 
 export default function Home() {
   return (
-    
-
-      <>
-      <Capture/>
-    {/* <Header/> */}
-    {/* <Onepage/> */}
+    <>
+      <Capture />
+      {/* <Header/> */}
+      {/* <Onepage/> */}
     </>
-    
-
   );
 }
